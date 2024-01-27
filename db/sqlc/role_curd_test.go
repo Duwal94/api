58c736357a455bf0ff23f9f50f6e54d4472a843f
Duwal <riyeshshrestha1094@gmail.com>
2024-01-27 16:50:42 +0545

    feat: random add

---
+++ db/sqlc/role_curd_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"context"
+	"database/sql" // Import the sql package to use sql.NullString
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateRole(t *testing.T) {
+	arg := CreateRoleParams{
+		RoleName:  sql.NullString{String: "Admin", Valid: true},
+		UpdatedBy: sql.NullString{String: "riyesh", Valid: true},
+		CreatedBy: sql.NullString{String: "riyesh", Valid: true},
+	}
+	role, err := testQueries.CreateRole(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, role)
+
+	require.Equal(t, arg.RoleName, role.RoleName)
+	require.Equal(t, arg.UpdatedBy, role.UpdatedBy)
+
+	require.NotZero(t, role.ID)
+	require.NotZero(t, role.CreatedAt)
+	require.NotZero(t, role.UpdatedAt)
+	require.NotZero(t, role.CreatedBy)
+}
--- db/sqlc/tempCodeRunnerFile.go
+++
@@ -1,9 +0,0 @@
-func TestMain(m *testing.M){
-	conn, err :=sql.Open(dbDriver,dbSource)
-	if err != nil{
-		log.Fatal("cannot connect to db:",err)
-	}
-	testQueries	= New(conn)
-
-	os.Exit(m.Run())
-}
